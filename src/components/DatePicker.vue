8dfd46542b2321675d26adfa6b42b5e41c13c72b
Marvin Schürz <marvin.schuerz@offisy.at>
2020-11-26 16:47:40 +0100

    changed popper behaviour

--- src/components/DatePicker.vue
+++ src/components/DatePicker.vue
@@ -7,7 +7,8 @@
     </div>
 
     <div class="calendar-popover" v-show="popoverShowing" ref="popover">
-      <calendar :calendar-width="calendarWidth" :calendar-height="calendarHeight" :type="type" :value="localValue" :month.sync="month" :year.sync="year" @input="localValue = $event">
+      <calendar :calendar-width="calendarWidth" :calendar-height="calendarHeight" :type="type" :value="localValue"
+                :month.sync="month" :year.sync="year" @input="localValue = $event">
 
         <template v-for="(_, name) of $scopedSlots" v-slot:[name]="scope">
           <slot :name="name" v-bind="scope"></slot>
@@ -39,7 +40,7 @@ import { Component, Prop, Ref, Vue, Watch } from 'vue-property-decorator'
 import { CalendarType, DateRange } from '@/lib'
 import { format, parse } from 'date-fns'
 import Calendar from '@/components/Calendar.vue'
-import { createPopper } from '@popperjs/core'
+import { createPopper, Instance } from '@popperjs/core'
 import { ClickOutside } from '@/directives/ClickOutside'
 
 @Component({
@@ -63,13 +64,14 @@ export default class DatePicker extends Vue {
 
   @Ref() input!: HTMLElement;
   @Ref() popover!: HTMLElement;
+  private popper: Instance;
 
   mounted () {
     this.localValue = this.value
     this.formatValue()
 
     this.$nextTick(() => {
-      createPopper(this.input, this.popover, {
+      this.popper = createPopper(this.input, this.popover, {
         placement: 'bottom-start',
       })
     })
@@ -108,6 +110,7 @@ export default class DatePicker extends Vue {
     this.month = date.getMonth()
     this.year = date.getFullYear()
     this.popoverShowing = true
+    this.popper.forceUpdate()
   }
 
   closeModal () {
@@ -115,6 +118,7 @@ export default class DatePicker extends Vue {
       this.closeTimeout = null
       this.popoverShowing = false
       this.localValue = this.value
+      this.popper.forceUpdate()
     }, 50)
   }
 
@@ -235,6 +239,10 @@ export default class DatePicker extends Vue {
   discard () {
     this.localValue = this.value
     this.closeModal()
+  }
+
+  beforeDestroy () {
+    this.popper.destroy()
   }
 }
 
